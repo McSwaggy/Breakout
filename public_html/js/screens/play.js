7b9e4e755e8fd8b29db98fe7da52493e698f4ddc
McSwaggy <joshxl14@yahoo.com>
2014-03-05 08:11:07 -0800

    initial comment
    initial comment of the Breakout melonJS  game created for Web
    Development 1

---
+++ public_html/js/screens/play.js
@@ -0,0 +1,25 @@
+game.PlayScreen = me.ScreenObject.extend({
+	/**
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {
+		// reset the score
+		game.data.score = 0;
+                // *** App Academy ***
+                // Load the level you created in the program Tiled
+                 me.levelDirector.loadLevel("level6");
+                 var ball = new game.BallEntity(50, 200,{});
+                 me.game.add(ball,  4),
+		// add our HUD to the game world
+		this.HUD = new game.HUD.Container();
+		me.game.world.addChild(this.HUD);
+	},
+
+
+	/**
+	 *  action to perform when leaving this screen (state change)
+	 */                     
+	onDestroyEvent: function() {             // remove the HUD from the game world\\``  [wqpoiu7
+             me.game.world.removeChild(this.HUD);
+        }
+});
